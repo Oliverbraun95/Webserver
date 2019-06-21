d94fd64f4d3f26d92fc43be9ea3d572554d2d9a8
Oliver <oliversum034@gmail.com>
2019-06-21 18:37:11 +0200

    Help me

--- myFirst.js
+++ myFirst.js
@@ -1,7 +1,12 @@
+var fs = require("fs");
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    fs.readFile('Hello.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
   });
 const server = app.listen(8080, () => {
     console.log(`Express running → PORT ${server.address().port}`);
