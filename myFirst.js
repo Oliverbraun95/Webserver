23b050b432f44df9a78a6f2b8ae05e5ede12b19e
Oliver <oliversum034@gmail.com>
2019-06-21 19:46:21 +0200

    HAHA

--- myFirst.js
+++ myFirst.js
@@ -1,13 +1,12 @@
-var fs = require("fs");
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    fs.readFile('Hello.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
+    res.render('index', {
+        title: 'Homepage'
+    });
       });
-  });
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 const server = app.listen(8080, () => {
     console.log(`Express running → PORT ${server.address().port}`);
   });
